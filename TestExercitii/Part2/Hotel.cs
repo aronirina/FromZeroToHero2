aa21fb749381e9bed1d8f2b178e49ff3d06803bb
aronirina <aron.irinagabriela@gmail.com>
2015-05-13 11:13:02 +0300

    Part2

---
+++ TestExercitii/Part2/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercitii.Part2
+{
+    class Hotel
+    {
+        public string name, description, address;
+        public int stars;
+        public double distanceToCenter;
+        public DateTime openingDate;
+        public Room[] rooms;
+
+       
+    }
+}
---
+++ TestExercitii/Part2/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercitii.Part2
+{
+    class Room
+    {
+        public string description;
+        public int places, number, floor;
+        RoomTypes type;
+
+        public enum RoomTypes
+        {
+            Single,
+            Double,
+            Twin,
+            Duplex,
+            KingBedroom
+        }
+
+    }
+  
+}
