5447df0dbc9356565ef48ebac1e6f37beccfd56c
aronirina <aron.irinagabriela@gmail.com>
2015-05-15 12:01:37 +0300

    MVC

---
+++ FZTH.MVC/Controllers/FooController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FZTH.MVC.Controllers
+{
+    public class FooController : Controller
+    {
+        //
+        // GET: /Foo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
